4375bb4d65397619afa56dda1537b4f4112e7a71
ross daly <rossdaly@gmail.com>
2018-09-25 01:10:57 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -16,6 +16,6 @@ function divSelector(){
 }
 
 function firstListItem(){
-  console.log($('ul #pic-list:first-child'))
+  // console.log($('ul #pic-list:first-child'))
   return  $('#pic-list li:first-child')[0]
 }
